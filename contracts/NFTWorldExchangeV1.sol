58c739bfea743886c09856cd11af25e4408e0f95
Matthew Watman <mattwatman@gmail.com>
2021-10-12 11:48:52 -0400

    Add interface import

--- contracts/NFTWorldExchangeV1.sol
+++ contracts/NFTWorldExchangeV1.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
+import "./INFTWorldExchange.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol;";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
