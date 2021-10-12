9061e0aceebfeb1a6a84c19caa085e5cb390ecc3
Matthew Watman <mattwatman@gmail.com>
2021-10-12 11:08:21 -0400

    Add token receiver import

--- contracts/NFTWorldExchangeV1.sol
+++ contracts/NFTWorldExchangeV1.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol;";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable";
 
