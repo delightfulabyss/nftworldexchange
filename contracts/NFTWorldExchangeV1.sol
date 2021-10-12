2fa0d908c29fc06155852f6204527b20b3b73572
Matthew Watman <mattwatman@gmail.com>
2021-10-12 11:12:04 -0400

    Add initializer import

--- contracts/NFTWorldExchangeV1.sol
+++ contracts/NFTWorldExchangeV1.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol;";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 contract NFTWorldExchangeImplmentationV1 {
 
