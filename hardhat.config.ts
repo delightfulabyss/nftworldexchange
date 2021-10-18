a6a906aa2706c6d0b4979f58a736986db8dcff6f
Matthew Watman <mattwatman@gmail.com>
2021-10-18 11:19:32 -0400

    Configure hardhat for mainnet forking

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+    hardhat: {
+      forking: {
+        url: process.env.POLYGON_URL || "",
+      },
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
