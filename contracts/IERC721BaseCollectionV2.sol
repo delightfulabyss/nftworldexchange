b56d145b362412dd3727a846d78c5d48e388d02b
Matthew Watman <mattwatman@gmail.com>
2021-10-18 14:51:29 -0400

    Fix compilation error

--- contracts/IERC721BaseCollectionV2.sol
+++ contracts/IERC721BaseCollectionV2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-interface ERC721BaseCollectionV2 {
+interface IERC721BaseCollectionV2 {
     function safeBatchTransferFrom(address _from, address _to, uint256[] memory _tokenIds, bytes memory _data) external;
 }
