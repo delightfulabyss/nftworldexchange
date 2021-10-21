977276d6ffd7f852d367a44da69f8edd521d90d8
Matthew Watman <mattwatman@gmail.com>
2021-10-21 11:37:53 -0400

    Add deploy script logic

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import hre, { ethers } from "hardhat";
+import hre, { ethers, upgrades } from "hardhat";
 
 async function main() {
   await hre.network.provider.request({
@@ -11,13 +11,22 @@ async function main() {
     params: ["0xd5e9ef1cedad0d135d543d286a2c190b16cbb89e"],
   });
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NFTWorldExchange = await ethers.getContractFactory(
+    "NFTWorldExchangeImplmentationV1"
+  );
 
-  await greeter.deployed();
+  const NFTWorldExchangeProxy = await upgrades.deployProxy(NFTWorldExchange, [
+    "0xcae8304fa1f65bcd72e5605db648ee8d6d889509",
+    "0xd5e9ef1cedad0d135d543d286a2c190b16cbb89e",
+    ["0x13166638AD246fC02cf2c264D1776aEFC8431B76"],
+  ]);
 
-  console.log("Greeter deployed to:", greeter.address);
+  await NFTWorldExchangeProxy.deployed();
+
+  console.log(
+    "NFTWorldExchangeProxy deployed to:",
+    NFTWorldExchangeProxy.address
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
