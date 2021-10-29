265d37a530b2e8d08b7bf2da1c9877ecc0cb095f
Matthew Watman <mattwatman@gmail.com>
2021-10-29 17:48:34 -0400

    Change default network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
-  defaultNetwork: "localhost",
+  defaultNetwork: "mumbai",
   networks: {
     hardhat: {
       forking: {
